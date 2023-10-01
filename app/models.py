13eacbb0e41a191420ef015ffbb978ccf7ee6e99
cristian-rincon <cristian.o.rincon.b@gmail.com>
2023-09-30 21:49:06 -0500

    refactor: improving code structure

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel
+from database import Base
+
+
+class Car(Base):
+    __tablename__ = "cars"
+    id = Column(Integer, primary_key=True, index=True)
+    brand = Column(String, index=True)
+    model = Column(String, index=True)
+    year = Column(Integer, index=True)
+
+
+# Pydantic models
+class CarCreate(BaseModel):
+    brand: str
+    model: str
+    year: int
+
+    class Config:
+        orm_mode = True
+
+
+class CarResponse(CarCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
